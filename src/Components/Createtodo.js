b0cab42fac30bcda63e50380af54083452132958
Johan Koekemoer <johanadamkoekemoer@gmail.com>
2023-06-27 12:23:34 +0200

    Hidden firebaseConfig

--- src/Components/Createtodo.js
+++ src/Components/Createtodo.js
@@ -1,12 +1,18 @@
+// Import the useState hook and Firebase functions
 import React, { useState } from 'react';
 import { db } from '../Firebase';
 import { collection, addDoc } from 'firebase/firestore';
 
-function CreateTodo(props) {
+// CreateTodo functional component
+function CreateTodo() {
+
+    // Declare state variables
     const [Subject, setSubject] = useState("");
+
+    // HandleSubmit function that adds a new todo when `add-todo` is clicked, then clears the input field
     async function handleSubmit (event) {
-        event.preventDefault();
-        if (Subject !== "") {
+        event.preventDefault(); // Prevents the default page refresh caused by the submit event
+        if (Subject !== "") { // Function only continues if Subject is not empty
             await addDoc(collection(db, "todos"), {
                 Subject,
                 completed: false,
@@ -14,6 +20,8 @@ function CreateTodo(props) {
             setSubject("");
         }
     }
+
+    // JSX structure for CreateTodo
     return (
         <form onSubmit={handleSubmit}>
             <div className="input-container">
--- src/Firebase.js
+++ src/Firebase.js
@@ -3,13 +3,13 @@ import { getFirestore } from "firebase/firestore"
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB6ezBp256iSR6ILgZDhjg1PJwqorIjADs",
-  authDomain: "todo-app-292be.firebaseapp.com",
-  databaseURL: "https://todo-app-292be-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "todo-app-292be",
-  storageBucket: "todo-app-292be.appspot.com",
-  messagingSenderId: "322417231528",
-  appId: "1:322417231528:web:3b39b0dfeb8af59a99249d"
+  apiKey: "XXXXXXXXXXXXXXXXXXXX",
+  authDomain: "XXXXXXXXXXXXXXXXXXXX",
+  databaseURL: "XXXXXXXXXXXXXXXXXXXX",
+  projectId: "XXXXXXXXXXXXXXXXXXXX",
+  storageBucket: "XXXXXXXXXXXXXXXXXXXX",
+  messagingSenderId: "XXXXXXXXXXXXXXXXXXXX",
+  appId: "XXXXXXXXXXXXXXXXXXXX"
 };
 
 const app = initializeApp(firebaseConfig)
